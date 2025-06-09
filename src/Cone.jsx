9f3925628f97a815703ba14f596896a4cc83eefd
farazzshaikh <farazzshaikh@gmail.com>
2025-06-09 19:30:11 +0400

    Update submodule demo-2022-porsche

--- src/Cone.jsx
+++ src/Cone.jsx
@@ -1,8 +1,16 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF } from "@react-three/drei";
 
 export default function Cone({ ...props }) {
-  const { nodes, materials } = useGLTF('/cone-transformed-v1.glb')
-  return <mesh dispose={null} castShadow geometry={nodes.Aset_other__M_uhrhabqfa_LOD0.geometry} material={materials.DefaultMaterial} {...props} />
+  const { nodes, materials } = useGLTF(
+    "/demo-2022-porsche/cone-transformed-v1.glb"
+  );
+  return (
+    <mesh
+      dispose={null}
+      castShadow
+      geometry={nodes.Aset_other__M_uhrhabqfa_LOD0.geometry}
+      material={materials.DefaultMaterial}
+      {...props}
+    />
+  );
 }
-
-useGLTF.preload('/cone-transformed-v1.glb')
